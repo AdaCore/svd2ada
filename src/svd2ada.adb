093d56dea852b4bcdca3bd131345b2316796e528
rogers <rogers@adacore.com>
2020-05-30 11:56:05 -0500

    Mention that --no-vfa-on-types forces --no-arrays. Shorten messages shown for --help. Refactor main procedure.

--- src/svd2ada.adb
+++ src/svd2ada.adb
@@ -2,7 +2,7 @@
 --                                                                          --
 --                          SVD Binding Generator                           --
 --                                                                          --
---                    Copyright (C) 2015-2018, AdaCore                      --
+--                    Copyright (C) 2015-2020, AdaCore                      --
 --                                                                          --
 -- SVD2Ada is free software;  you can  redistribute it  and/or modify it    --
 -- under terms of the  GNU General Public License as published  by the Free --
@@ -20,10 +20,10 @@
 with Ada.Command_Line;
 with Ada.Exceptions;
 with Ada.Strings.Unbounded;       use Ada.Strings.Unbounded;
-with Ada.Text_IO;
+with Ada.Text_IO;                 use Ada.Text_IO;
 
 with GNAT.Command_Line;
-with GNAT.Directory_Operations;
+with GNAT.Directory_Operations;   use GNAT.Directory_Operations;
 with GNAT.OS_Lib;
 with GNAT.Strings;
 
@@ -38,7 +38,7 @@ with DOM.Core.Documents;
 
 with Ada_Gen;
 with Descriptors.Device;
-with SVD2Ada_Utils;
+with SVD2Ada_Utils;               use SVD2Ada_Utils;
 
 --  SVD Binding Generator: this tool is meant to handle
 --  A SVD file is an xml file representing a specific hardware device, and
@@ -58,27 +58,22 @@ with SVD2Ada_Utils;
 --    of the valid values that are expected for a given field. If such
 --    element is present, then the binding generator will create an enum
 --    for the field, and use this enum as type of the field.
-procedure SVD2Ada
-is
-   --  Local variables used for XML parsing
-   Input           : File_Input;
-   Reader          : Tree_Reader;
-   Sc_Reader       : Schema_Reader;
-   Grammar         : XML_Grammar;
-   Doc             : Document;
-   Schema          : constant String :=
-                       GNAT.OS_Lib.Normalize_Pathname
+
+procedure SVD2Ada is
+
+   Doc    : Document;
+   Schema : constant String := GNAT.OS_Lib.Normalize_Pathname
                          ("schema/CMSIS-SVD_Schema_1_3_1.xsd",
-                          SVD2Ada_Utils.Executable_Location);
+                          Executable_Location);
 
-   --  The produced Device
+   --  The generated Device
    Device          : Descriptors.Device.Device_T;
-   SVD_File        : Unbounded_String;
+   SVD_File_Name   : Unbounded_String;
 
-   --  Command line parser
+   --  Command line parser and switches/arguments
    Cmd_Line_Cfg    : GNAT.Command_Line.Command_Line_Configuration;
    Root_Pkg_Name   : aliased GNAT.Strings.String_Access;
-   Out_Dir         : aliased GNAT.Strings.String_Access;
+   Output_Dir      : aliased GNAT.Strings.String_Access;
    Base_Types_Pkg  : aliased GNAT.Strings.String_Access;
    Gen_Booleans    : aliased Boolean := False;
    Gen_UInt_Always : aliased Boolean := False;
@@ -90,180 +85,220 @@ is
 
    use type GNAT.Strings.String_Access;
 
-begin
-   GNAT.Command_Line.Set_Usage
-     (Cmd_Line_Cfg,
-      Usage => "[options] -o DIR svd-file",
-      Help  => "Generate Ada bindings from CMSIS-SVD Cortex-M hardware " &
-        "description files");
-   GNAT.Command_Line.Define_Switch
-     (Cmd_Line_Cfg,
-      Output      => Out_Dir'Access,
-      Switch      => "-o=",
-      Long_Switch => "--output=",
-      Help        => "the directory to contain the generated binding",
-      Argument    => "DIR");
-   GNAT.Command_Line.Define_Switch
-     (Cmd_Line_Cfg,
-      Output      => Root_Pkg_Name'Access,
-      Switch      => "-p=",
-      Long_Switch => "--package=",
-      Help        => "use this name as root package name for the generated " &
-        "spec hierarchy",
-      Argument    => "Pkg_Name");
-   GNAT.Command_Line.Define_Switch
-     (Cmd_Line_Cfg,
-      Output      => Base_Types_Pkg'Access,
-      Long_Switch => "--base-types-package=",
-      Help        => "the name of the package containing the low level types'" &
-        " definitions. If not specified, those types will be declared in the" &
-        " root package.");
-   GNAT.Command_Line.Define_Switch
-     (Cmd_Line_Cfg,
-      Output      => Gen_Booleans'Access,
-      Long_Switch => "--boolean",
-      Help        => "represent single-bit fields as Booleans. Ignored if an enumeration " &
-        "is defined for the field",
-      Value       => True);
-   GNAT.Command_Line.Define_Switch
-     (Cmd_Line_Cfg,
-      Output      => Gen_IRQ_Support'Access,
-      Long_Switch => "--gen-interrupts",
-      Help        => "generate trap handlers and interrupt names package. " &
-        "Activated by default if the generated root package is a run-time " &
-        "package",
-      Value       => True);
-   GNAT.Command_Line.Define_Switch
-     (Cmd_Line_Cfg,
-      Output      => Gen_UInt_Always'Access,
-      Long_Switch => "--gen-uint-always",
-      Help        => "when generating base types, consider UInt* and" &
-        " do not use the Bit and Byte variants for types with size 1 and 8",
-      Value       => True);
-   GNAT.Command_Line.Define_Switch
-     (Cmd_Line_Cfg,
-      Output      => No_Arrays'Access,
-      Long_Switch => "--no-arrays",
-      Help        => "in some circumstances (e.g., similar names indexed by " &
-        "ascending numbers), svd2ada can generate arrays of registers or " &
-        "arrays of register fields. This switch prevents this behavior",
-      Value       => True);
-   GNAT.Command_Line.Define_Switch
-     (Cmd_Line_Cfg,
-      Output      => No_Defaults'Access,
-      Long_Switch => "--no-defaults",
-      Help        => "do not use the registers' reset values to provide" &
-        " fields' default values",
-      Value       => True);
-   GNAT.Command_Line.Define_Switch
-     (Cmd_Line_Cfg,
-      Output      => No_UInt_Subtype'Access,
-      Long_Switch => "--no-uint-subtypes",
-      Help        => "do not generate subtypes for fields, but use the corresponding" &
-        " UInt type instead",
-      Value       => True);
-   GNAT.Command_Line.Define_Switch
-     (Cmd_Line_Cfg,
-      Output      => No_VFA_On_Types'Access,
-      Long_Switch => "--no-vfa-on-types",
-      Help        => "when generating register types, do not specify the" &
-        " Volatile_Full_Access aspect on them (specify it instead on the" &
-        " enclosing peripheral fields)",
-      Value       => True);
+   procedure Configure_Command_Line;
+   --  Define the switches used on the command line
 
-   GNAT.Command_Line.Getopt
-     (Config => Cmd_Line_Cfg);
+   procedure Apply_Command_Line;
+   --  Set the generation switches and options based on command line settings
 
-   declare
+   procedure Get_SVD_File_Name (Name : out Unbounded_String);
+   --  Get the normalized XML input file name
+
+   procedure Parse_XML_Files
+     (File_Name : String;
+      Schema    : String;
+      Doc       : out Document);
+   --  Parse the XML in the SVD input file and check for a helper file to
+   --  give us hints as to how generate some structures
+
+   ----------------------------
+   -- Configure_Command_Line --
+   ----------------------------
+
+   procedure Configure_Command_Line is
+      use GNAT.Command_Line;
+   begin
+      Set_Usage
+        (Cmd_Line_Cfg,
+         Usage => "[options] -o DIR svd-file",
+         Help  => "Generate Ada bindings from CMSIS-SVD hardware description files");
+      Define_Switch
+        (Cmd_Line_Cfg,
+         Output      => Output_Dir'Access,
+         Switch      => "-o=",
+         Long_Switch => "--output=",
+         Help        => "directory to contain the generated binding",
+         Argument    => "DIR");
+      Define_Switch
+        (Cmd_Line_Cfg,
+         Output      => Root_Pkg_Name'Access,
+         Switch      => "-p=",
+         Long_Switch => "--package=",
+         Help        => "the root package name for the generated spec hierarchy",
+         Argument    => "Pkg_Name");
+      Define_Switch
+        (Cmd_Line_Cfg,
+         Output      => Base_Types_Pkg'Access,
+         Long_Switch => "--base-types-package=",
+         Help        => "the name of the package containing low level types' definitions");
+      Define_Switch
+        (Cmd_Line_Cfg,
+         Output      => Gen_Booleans'Access,
+         Long_Switch => "--boolean",
+         Help        => "represent single-bit fields as Booleans",
+         Value       => True);
+      Define_Switch
+        (Cmd_Line_Cfg,
+         Output      => Gen_IRQ_Support'Access,
+         Long_Switch => "--gen-interrupts",
+         Help        => "generate trap handlers and package Interrupts.Names",
+         Value       => True);
+      Define_Switch
+        (Cmd_Line_Cfg,
+         Output      => Gen_UInt_Always'Access,
+         Long_Switch => "--gen-uint-always",
+         Help        => "generate UInt* for base types; do not use the Bit and Byte variants",
+         Value       => True);
+      Define_Switch
+        (Cmd_Line_Cfg,
+         Output      => No_Arrays'Access,
+         Long_Switch => "--no-arrays",
+         Help        => "don't generate arrays of registers or arrays of register fields",
+         Value       => True);
+      Define_Switch
+        (Cmd_Line_Cfg,
+         Output      => No_Defaults'Access,
+         Long_Switch => "--no-defaults",
+         Help        => "don't use register reset values for fields' default values",
+         Value       => True);
+      Define_Switch
+        (Cmd_Line_Cfg,
+         Output      => No_UInt_Subtype'Access,
+         Long_Switch => "--no-uint-subtypes",
+         Help        => "use the corresponding UInt type instead of subtypes for fields",
+         Value       => True);
+      Define_Switch
+        (Cmd_Line_Cfg,
+         Output      => No_VFA_On_Types'Access,
+         Long_Switch => "--no-vfa-on-types",
+         Help        => "apply Volatile_Full_Access to enclosing peripheral fields. Forces --no-arrays",
+         Value       => True);
+   end Configure_Command_Line;
+
+   ------------------------
+   -- Apply_Command_Line --
+   ------------------------
+
+   procedure Apply_Command_Line is
+   begin
+      if Root_Pkg_Name.all /= "" then
+         Set_Root_Package (Root_Pkg_Name.all);
+
+         --  If Pkg is a runtime package, force the generation of Interrupts.Names
+         --  and IRQ trap vector.
+         if SVD2Ada_Utils.In_Runtime then
+            Gen_IRQ_Support := True;
+         end if;
+      end if;
+
+      Set_Use_Boolean_For_Bit (Gen_Booleans);
+      Set_Gen_IRQ_Support (Gen_IRQ_Support);
+      Set_Use_UInt (Gen_UInt_Always);
+      Set_Gen_Arrays (not No_Arrays);
+      Set_No_Defaults (No_Defaults);
+      Set_No_UInt_Subtype (No_UInt_Subtype);
+      Set_No_VFA_On_Reg_Types (No_VFA_On_Types);
+
+      if Base_Types_Pkg.all /= "" then
+         Set_Base_Types_Package (Base_Types_Pkg.all);
+      end if;
+   end Apply_Command_Line;
+
+   -----------------------
+   -- Get_SVD_File_Name --
+   -----------------------
+
+   procedure Get_SVD_File_Name (Name : out Unbounded_String) is
       SVD : constant String := GNAT.Command_Line.Get_Argument;
    begin
-      if SVD = "" or else Out_Dir = null or else Out_Dir.all = "" then
-         Ada.Text_IO.Put_Line (Ada.Text_IO.Standard_Error, "Error: missing arguments");
-         GNAT.Command_Line.Try_Help;
-
-         Ada.Command_Line.Set_Exit_Status (2);
-         return;
+      if SVD /= "" then
+         Name := To_Unbounded_String (GNAT.OS_Lib.Normalize_Pathname (SVD));
       end if;
+   end Get_SVD_File_Name;
 
-      SVD_File := To_Unbounded_String (GNAT.OS_Lib.Normalize_Pathname (SVD));
-   end;
+   ---------------------
+   -- Parse_XML_Files --
+   ---------------------
 
-   if Root_Pkg_Name.all /= "" then
-      SVD2Ada_Utils.Set_Root_Package (Root_Pkg_Name.all);
+   procedure Parse_XML_Files
+     (File_Name : String;
+      Schema    : String;
+      Doc       : out Document)
+   is
+      Input           : File_Input;
+      Reader          : Tree_Reader;
+      Sc_Reader       : Schema_Reader;
+      Grammar         : XML_Grammar;
+   begin
+      --  Open the schema file
+      Input_Sources.File.Open (Schema, Input);
+      Sc_Reader.Parse (Input);
+      Close (Input);
+      Grammar := Get_Grammar (Sc_Reader);
 
-      --  If Pkg is a runtime package, force the generation of Interrupts.Name
-      --  and IRQ trap vector.
-      if SVD2Ada_Utils.In_Runtime then
-         Gen_IRQ_Support := True;
-      end if;
-   end if;
-
-   SVD2Ada_Utils.Set_Use_Boolean_For_Bit (Gen_Booleans);
-   SVD2Ada_Utils.Set_Gen_IRQ_Support (Gen_IRQ_Support);
-   SVD2Ada_Utils.Set_Use_UInt (Gen_UInt_Always);
-   SVD2Ada_Utils.Set_Gen_Arrays (not No_Arrays);
-   SVD2Ada_Utils.Set_No_Defaults (No_Defaults);
-   SVD2Ada_Utils.Set_No_UInt_Subtype (No_UInt_Subtype);
-   SVD2Ada_Utils.Set_No_VFA_On_Reg_Types (No_VFA_On_Types);
-
-   if Base_Types_Pkg.all /= "" then
-      SVD2Ada_Utils.Set_Base_Types_Package (Base_Types_Pkg.all);
-   end if;
-
-   Ada_Gen.Set_Input_File_Name
-     (GNAT.Directory_Operations.Base_Name (To_String (SVD_File)));
-
-   --  Open the schema file
-   Input_Sources.File.Open (Schema, Input);
-   Sc_Reader.Parse (Input);
-   Close (Input);
-   Grammar := Get_Grammar (Sc_Reader);
-
-   --  tell the reader to use the schema to validate the SVD file
-   Reader.Set_Grammar (Grammar);
-   Set_Feature (Reader, Sax.Readers.Schema_Validation_Feature, False);
-   Use_Basename_In_Error_Messages (Reader, True);
-
-   Input_Sources.File.Open (To_String (SVD_File), Input);
-   Reader.Parse (Input);
-   Close (Input);
-
-   Doc := Get_Tree (Reader);
-
-   --  Check if we have some helper file to give us hints as to how generate
-   --  some structures. Rule: if we have both XXXX.svd and XXXX.svd2ada, then
-   --  the svd2ada file is our helper file
-   if GNAT.OS_Lib.Is_Regular_File (To_String (SVD_File) & "2ada") then
-      Input_Sources.File.Open (To_String (SVD_File) & "2ada", Input);
-
+      --  Tell the reader to use the schema to validate the SVD file
+      Reader.Set_Grammar (Grammar);
       Set_Feature (Reader, Sax.Readers.Schema_Validation_Feature, False);
       Use_Basename_In_Error_Messages (Reader, True);
+
+      Input_Sources.File.Open (File_Name, Input);
       Reader.Parse (Input);
       Close (Input);
+
+      Doc := Get_Tree (Reader);
+
+      --  Check if we have a helper file to give us hints as to how generate
+      --  some structures. Rule: if we have both XXXX.svd and XXXX.svd2ada,
+      --  then the svd2ada file is our helper file
+      if GNAT.OS_Lib.Is_Regular_File (File_Name & "2ada") then
+         Input_Sources.File.Open (File_Name & "2ada", Input);
+         Set_Feature (Reader, Sax.Readers.Schema_Validation_Feature, False);
+         Use_Basename_In_Error_Messages (Reader, True);
+         Reader.Parse (Input);
+         Close (Input);
+      end if;
+   exception
+      when XML_Validation_Error =>
+         Close (Input);
+         Put_Line ("Invalid SVD file:");
+         Put_Line (Reader.Get_Error_Message);
+         raise;
+      when E : Sax.Readers.XML_Fatal_Error =>
+         Close (Input);
+         Put_Line ("Fatal error when parsing the SVD file:");
+         Put_Line (Ada.Exceptions.Exception_Message (E));
+         raise;
+   end Parse_XML_Files;
+
+begin
+   Configure_Command_Line;
+   GNAT.Command_Line.Getopt (Cmd_Line_Cfg);
+   Get_SVD_File_Name (SVD_File_Name);
+   if SVD_File_Name = Null_Unbounded_String or else
+      Output_Dir = null or else Output_Dir.all = ""
+   then
+      Put_Line (Standard_Error, "Error: missing arguments");
+      GNAT.Command_Line.Try_Help;
+      Ada.Command_Line.Set_Exit_Status (2);
+      return;
    end if;
+   Apply_Command_Line;
+
+   Ada_Gen.Set_Input_File_Name (Base_Name (To_String (SVD_File_Name)));
+
+   Parse_XML_Files (To_String (SVD_File_Name), Schema, Doc);
 
    Device := Descriptors.Device.Read_Device
      (Documents.Get_Element (Doc),
       (if Root_Pkg_Name = null then "" else Root_Pkg_Name.all));
 
-   Descriptors.Device.Dump (Device, Out_Dir.all);
-
+   Descriptors.Device.Dump (Device, Output_Dir.all);
 exception
    when GNAT.Command_Line.Invalid_Switch |
         GNAT.Command_Line.Invalid_Parameter |
         GNAT.Command_Line.Exit_From_Command_Line =>
       Ada.Command_Line.Set_Exit_Status (1);
 
-   when XML_Validation_Error =>
-      Close (Input);
-      Ada.Text_IO.Put_Line ("Non-valid SVD file:");
-      Ada.Text_IO.Put_Line (Reader.Get_Error_Message);
+   when XML_Validation_Error | Sax.Readers.XML_Fatal_Error =>
       Ada.Command_Line.Set_Exit_Status (2);
-
-   when E : Sax.Readers.XML_Fatal_Error =>
-      Close (Input);
-      Ada.Text_IO.Put_Line ("Fatal error when parsing the svd file:");
-      Ada.Text_IO.Put_Line (Ada.Exceptions.Exception_Message (E));
-      Ada.Command_Line.Set_Exit_Status (2);
-
 end SVD2Ada;
