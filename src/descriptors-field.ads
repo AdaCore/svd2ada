a0f8942eaabe1b435f7cce20d58cec459b8429b0
Simon Wright <simon@pushface.org>
2024-09-03 14:21:03 +0100

    Process dimensioned record fields.
    From the SVD, field components have an optional <dimElementGroup>:
      <dim> specifies the number of array elements,
      <dimIncrement> specifies the address (?bit) offset between consecutive
        array elements,
      (optional) <dimIndex> specifies "a comma seperated list of
        strings being used for identifying each element in the array",
        but we only see r'[0-9]+\-[0-9]+'.
    
    We will produce <dim> fields, where %s in the Name translates to the
    first component of <dimIndex> incremented by 1 for each field, and
    LSB, MSB are incremented by <dimIncrement>.
    
      * src/descriptors-field.ads
        (Field_T): added components
          Dimensions: contents of the <dim> field in the SVD,
          Increment: contents of the <dimIncrement> field in the SVD,
          Index: the first component of the <dimIndex> field in the SVD.
        (Null_Field): added values for the new components, using associations.
      * src/descriptors-field.adb:
        (context): with Ada.Strings.Fixed, so we can pick out which Trim is
          required.
        (Read_Field): initialize new fields in Result. In particular, Dimensions
          will be set to 1. If present, process tags dim, dimIncrement, dimIndex.
        (Dump): add new function Insert_Dimensioned_Fields. Call this function
          on the input Reg_Fields, and then run the rest of Dump on the result.
        (Insert_Dimensioned_Fields): new. For each Field in the original, insert
          as many copies as indicated by its Dimensions, updating the Name, LSB
          and MSB. Note that if the original field from the SVD had no <dim>
          component, Dimensions will have been set to 1 by Read_Field, so this
          process will be done just once (resulting in no change).

--- src/descriptors-field.ads
+++ src/descriptors-field.ads
@@ -2,7 +2,7 @@
 --                                                                          --
 --                          SVD Binding Generator                           --
 --                                                                          --
---                    Copyright (C) 2015-2020, AdaCore                      --
+--                    Copyright (C) 2015-2024, AdaCore                      --
 --                                                                          --
 -- SVD2Ada is free software;  you can  redistribute it  and/or modify it    --
 -- under terms of the  GNU General Public License as published  by the Free --
@@ -32,9 +32,24 @@ with Descriptors.Enumerate;
 --  Decodes the <field> elements of the SVD file.
 package Descriptors.Field is
 
+   --  From the SVD, these components are in an optional <dimElementGroup>:
+   --  <dim> specifies the number of array elements,
+   --  <dimIncrement> specifies the address (?bit) offset between consecutive
+   --    array elements,
+   --  (optional) <dimIndex> specifies "a comma seperated list of
+   --    strings being used for identifying each element in the array",
+   --    but we only see r'[0-9]+\-[0-9]+'.
+   --
+   --  We will produce <dim> fields, where %s in the Name translates
+   --  to the first component of <dimIndex> incremented by 1 for each
+   --  field, and LSB, MSB are incremented by <dimIncrement>.
+
    type Field_T is record
       Name             : Unbounded.Unbounded_String;
       Description      : Unbounded.Unbounded_String;
+      Dimensions       : Natural;    -- <dim/>
+      Increment        : Natural;    -- <dimIncrement/>
+      Index            : Natural;    -- first component of <dimIndex/>
       LSB              : Natural;
       MSB              : Natural;
       Size             : Natural;
@@ -48,13 +63,16 @@ package Descriptors.Field is
    function "=" (F1, F2 : Field_T) return Boolean;
 
    Null_Field : constant Field_T :=
-                  (Unbounded.Null_Unbounded_String,
-                   Unbounded.Null_Unbounded_String,
-                   0,
-                   0,
-                   0,
-                   Read_Write,
-                   others => <>);
+     (Name        => Unbounded.Null_Unbounded_String,
+      Description => Unbounded.Null_Unbounded_String,
+      Dimensions  => 0,
+      Increment   => 0,
+      Index       => 0,
+      LSB         => 0,
+      MSB         => 0,
+      Size        => 0,
+      Acc         => Read_Write,
+      others      => <>);
 
    package Field_Vectors is new Ada.Containers.Vectors
      (Positive, Field_T);
